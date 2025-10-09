f90a251e6f0338457eeb00adaca0d1fa5623a4bd
Lucas Solhaune <lu.solhaune@gmail.com>
2025-10-09 19:19:28 -0300

    Datos escalares

---
+++ 03_datos_escalares.py
@@ -0,0 +1,38 @@
+#Int
+x = 5
+print(type(x))
+print(x)
+
+
+#Float
+x = 7.1
+print(type(x))
+print(x)
+
+
+#Bool
+es_verdadero = True
+print(type(es_verdadero))
+print(es_verdadero)
+
+
+#String
+saludo = "Hola, Lucas"
+print(type(saludo))
+print(saludo)
+
+
+#Complex
+x = 2 + 3j
+print(type(x))
+print(x)
+
+
+#Conversion de string a int
+x = '6'
+print(type(x))
+print(x)
+
+a = int(x) # Lo transformo a int
+print(type(a))
+print(a)
