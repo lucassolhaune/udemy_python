b6f9a51b7d2ec1e391f667fc332f3780135e9b76
Lucas Solhaune <lu.solhaune@gmail.com>
2025-10-09 19:18:13 -0300

    Estructuracion de python

---
+++ 01_estructura_programa.py
@@ -0,0 +1,32 @@
+import math
+
+print(math.sqrt(25))
+
+#Esto es un saludo
+print("Hola")
+
+A = "Lucas"
+E = 20
+
+def saludar(nombre):
+    print("Hola, " + nombre + "!")
+
+saludar(A) #Hola, Lucas!
+
+# if, else, elif
+# if edad >= 18:
+#     print("Es mayor de edad")
+# else:
+#     print("Es menor de edad")
+
+#Bucles
+for i in range(5):
+    print(i)
+
+
+# while edad < 30:
+#     print("Eres joven")
+#     edad += 1
+
+nombre = input("Ingrese su nombre: ")
+print("HOla," + nombre + "!")
