1c908b405e4824d850498361a0787db016949001
Lucas Solhaune <lu.solhaune@gmail.com>
2025-10-09 19:19:51 -0300

    Operadores aritmeticos

---
+++ 04_operadores_aritmeticos.py
@@ -0,0 +1,53 @@
+#########################################
+##Operadores
+
+#Suma
+suma = 3 + 4
+print("Suma: ", suma)
+
+#Resta
+resta = 4 - 3
+print("Resta: ", resta)
+
+#Multiplicacion
+multp = 3 * 4
+print("Multiplicacion: ", multp)
+
+#Division con flotante
+div = 12 / 4
+print("Division con flotante: ", div)
+
+#Division (entera), sin decimales
+div1 = 56 // 5
+print("Division entera: ", div1) 
+
+#Potencia
+pot = 3 ** 4
+print("Potencia: ", pot)
+
+#Modulo (resto de la division)
+mod = 10 % 3
+print("Modulo: ", mod)
+
+
+
+#######################################################
+#Operadores de comparacion
+
+a = 5 == 5
+print("Igual:", a)
+
+a = 5 != 5
+print("Distinto:", a)
+
+a = 3 < 5
+print("menor a:", a)
+
+a = 3 > 5
+print("mayor a:", a)
+
+a = 3 <= 5
+print("menor o igual a:", a)
+
+a = 3 >= 5
+print("mayor o igual a:",a)
