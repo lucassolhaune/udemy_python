c9094e2171d7b77e6e5f8521c3a66a8069dc0f6e
Lucas Solhaune <lu.solhaune@gmail.com>
2025-10-13 13:54:23 -0300

    input y ejemplos

---
+++ 14-input_print.py
@@ -0,0 +1,4 @@
+nombre = input("Ingrese su nombre: ")
+year = int(input("Ingrese el anio en el que nacio: "))
+
+print(f"Nombre: {nombre}, Edad: {2025-year}") #"2025-year" resta menos los anios que tengamos nosotros y nos dice cuando nacimos 
